54b05541011cde5eda8db444c3e543f2897a49dc
cassandra18 <cassandralelei013@gmail.com>
2023-07-07 21:58:43 +0300

    save and delete ontacts in  local storage

--- Front-end/contact-manager/src/components/AddContacts.js
+++ Front-end/contact-manager/src/components/AddContacts.js
@@ -1,43 +1,45 @@
 import React from "react";
 
 class AddContact extends React.Component {
+    //class state
     state = {
         name: "",
         email: "",
     };
 
-    app = (e) => {
+    add = (e) => {
         e.preventDefault();
-        if (this.state.name === "" && this.state.email === "") {
+        if (this.state.name === "" || this.state.email === "") {
             alert ("All fields are mandatory!");
-            return 
+            return;
         };
+        this.props.addContactHandler(this.state);
+        this.setState({ name: "", email: "" });
 
 
-
-    }
+    };
     render () {
         return (
             <div className="ui main">
                 <h2>Add Contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
                         <input type="text" 
                             name="name" 
                             placeholder="Name" 
                             value={this.state.name}
-                            onChange={(e) => this.state.name({name:e.target.value})}
+                            onChange={(e) => this.setState({name: e.target.value})}
                         ></input>
                     </div>
                     <div className="field">
                         <label>Email</label>
                         <input 
-                         type = "text" 
+                         type="text" 
                          name="email" 
                          placeholder="Email"
-                         value={this.state.name}
-                         onChange={(e) => this.state.name({name:e.target.value})}
+                         value={this.state.email}
+                         onChange={(e) => this.setState({email: e.target.value})}
                          ></input>
                     </div>
                     <button className="ui button blue">Add</button>
--- Front-end/contact-manager/src/components/App.js
+++ Front-end/contact-manager/src/components/App.js
@@ -1,16 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
+import { uuid } from 'uuidv4';
 import Header from "./Header"
 import AddContact  from "./AddContacts"
 import ContactList from "./ContactList"
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact}]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(newContactList);
+  }
+  useEffect(() => {
+    const retrievedContact = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrievedContact) setContacts(retrievedContact);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div>
+    <div className="ui container">
       <Header />
-      <AddContact />
-      <ContactList contacts = { contacts }/> 
+      <AddContact addContactHandler = { addContactHandler }/>
+      <ContactList contacts = { contacts } getContactId={removeContactHandler}/> 
     </div>
   );
 }
